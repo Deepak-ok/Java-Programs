2dc7109976bc22b1641f36750014ffc58e2a34ef
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-11 00:17:17 +0530

    first commit

---
+++ src/ArrayUtility.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayUtility {
+    public static int[] inputArray(){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please enter no. of Elements :");
+        int size=input.nextInt();
+        int[] nums=new int[size];
+        int i=0;
+        while (i<size){
+            System.out.print("Enter elements umber "+i+":");
+            nums[i]=input.nextInt();
+            i++;
+        }
+
+
+        return nums;
+    }
+}
