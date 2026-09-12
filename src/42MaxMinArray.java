0ae769b3665052e1c154ab43be9c233702002019
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-13 01:13:44 +0530

    .

--- src/42MaxMinArray.java
+++ src/42MaxMinArray.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+ class MaxMinArray {
+     static void main() {
+         System.out.println("Welcome to amx and min");
+         int[] numArr=ArrayUtility.inputArray();
+         int max= max(numArr);
+         int min= min(numArr);
+         System.out.println("Max of the array is: "+max);
+         System.out.println("Min of the array is : "+min);
+     }
 
-public class MaxMinArray {
+     public static int min(int[]  numArr){
+         int min=Integer.MAX_VALUE;
+         int i=0;
+         while (i<numArr.length){
+            if (min>numArr[i]){
+                min=numArr[i];
+            }
+             i++;
+         }
+         return min;
+     }
+
+     public static int max(int[]  numArr){
+         if (numArr.length==0){
+             return Integer.MAX_VALUE;
+         }
+         int max=numArr[0];
+         int i=1;
+         while (max< numArr.length){
+            if ( max<numArr[i]){
+                max=numArr[i];
+            }
+             i++;
+         }
+         return max;
+     }
 }
