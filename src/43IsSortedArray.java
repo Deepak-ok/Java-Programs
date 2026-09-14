cd781c82ee14d23e039f5f0cd95c34eddeeb4c6b
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-14 17:19:31 +0530

    .

--- src/43IsSortedArray.java
+++ src/43IsSortedArray.java
@@ -1,2 +1,41 @@
-public class IsSortedArray {
+import javax.xml.transform.Source;
+
+class IsSortedArray {
+     static void main() {
+         System.out.println("Welcome tp Array Sorting Check Post\n");
+         int[] numArr=ArrayUtility.inputArray();
+         boolean isInc=isIncreasing(numArr);
+         boolean IsDec=isDecreasing(numArr);
+
+         if (isInc || IsDec){
+             System.out.println("Your Array IS Sorted");
+         } else {
+             System.out.println("Your Array is not Sorted");
+         }
+
+     }
+
+     public static boolean isDecreasing(int[] numArr){
+         int i=1;
+         while (i < numArr.length){
+             if (numArr[i] < numArr[i-1]){
+                 return false;
+             }
+             i++;
+         }
+         return true;
+     }
+
+     public static boolean isIncreasing(int[] numArr){
+         int i=1;
+         while (i < numArr.length){
+             if (numArr[i] > numArr[i-1]){
+                 return false;
+             }
+             i++;
+         }
+         return true;
+
+     }
+
 }
