d00a4bb016ac3155686c412371892b85efc85419
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-17 00:52:39 +0530

    .

--- src/49SumAverage2DArray.java
+++ src/49SumAverage2DArray.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+ class SumAverage2DArray {
+     static void main() {
+         System.out.println("Welcome to sum and average 2D Array\n");
+         int[][] numArr=ArrayUtility.input2DArray();
+         long sum=sum(numArr);
+         double avg= average(numArr);
+         System.out.println("Sum Of array is :"+ sum);
+         System.out.println("Average Of Arry Is :"+ avg);
 
-public class SumAverage2DArray {
+     }
+
+     public static long sum(int[][] numArr){
+         long sum=0;
+         int i=0;
+         while (i< numArr.length){
+             int j=0;
+             while (j< numArr[i].length){
+                 sum+=numArr[i][j];
+                 j++;
+
+             }
+             i++;
+         }
+         return sum;
+
+     }
+     public static double average(int[][] numArr){
+       if (numArr.length==0){
+           return 0;
+       }
+         int rows= numArr.length;;
+         int cols=numArr[0].length;
+         double size=rows* cols;
+         return sum(numArr) / size ;
+
+     }
 }
