9845e24d02be48606b549a16b7c13cca25703e7d
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-17 00:32:07 +0530

    .

--- src/48Search2DArray.java
+++ src/48Search2DArray.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Search2DArray {
+class Search2DArray {
+     static void main() {
+         Scanner input= new Scanner(System.in);
+         System.out.println("Welcome to 2D Search");
+         int[][] numArr=ArrayUtility.input2DArray();
+         System.out.println("Now Enter The number you want to search");
+         int num= input.nextInt();
+         boolean isFound=search(numArr,num);
+         if (isFound){
+             System.out.println("Your number is found");
+         } else {
+             System.out.println("Your Number is Not found");
+         }
+
+     }
+
+     public static boolean search(int[][] numArr , int num){
+         int i=0;
+         while (i< numArr.length){
+             int j=0;
+             while (j< numArr[i].length){
+                 if(numArr[i][j] == num){
+                     return true;
+                 }
+                 j++;
+             }
+             i++;
+         }
+        return false;
+     }
 }
