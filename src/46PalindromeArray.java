bcb933f3133c8e4f134863f3884c012f180813b0
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-16 23:54:26 +0530

    .

--- src/46PalindromeArray.java
+++ src/46PalindromeArray.java
@@ -1,2 +1,23 @@
-public class PalindromeArray {
+ class PalindromeArray {
+     static void main() {
+         System.out.println("Welcome to Palindrome Checker");
+         int[] numArr=ArrayUtility.inputArray();
+         boolean isPalin=isPalindrome(numArr);
+         if (isPalin){
+             System.out.println("Your Array IS Palinddrome");
+         } else {
+             System.out.println("Your Array Is not Palindrome");
+         }
+     }
+
+     public static boolean isPalindrome(int[] numArr){
+         int i=0;
+         while (i< numArr.length/2){
+             if(numArr[i] != numArr[(numArr.length-1)-i]){
+                 return false;
+             }
+             i++;
+         }
+         return true;
+     }
 }
