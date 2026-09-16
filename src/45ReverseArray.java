9595b47fa5d60f8a4e04c4eb7b084998a1e69c1c
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-16 23:42:22 +0530

    .

--- src/45ReverseArray.java
+++ src/45ReverseArray.java
@@ -1,2 +1,20 @@
-public class ReverseArray {
+ class ReverseArray {
+     static void main() {
+         System.out.println("Welcome to Array Reverse");
+         int[] numArr =ArrayUtility.inputArray();
+         reverse(numArr);
+         System.out.println("Your reverse array is");
+         ArrayUtility.displayArray(numArr);
+
+     }
+
+     public static void reverse(int[] arr){
+      int i=0;
+      while (i< arr.length/2){
+          int swap=arr[i];
+          arr[i]=arr[(arr.length-1)-i];
+          arr[(arr.length-1)-i]=swap;
+          i++;
+      }
+     }
 }
