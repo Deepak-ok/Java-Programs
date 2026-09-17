b90fae3dbd92394330327334679e9ca901ad0095
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-18 00:27:52 +0530

    .

--- src/52Course.java
+++ src/52Course.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+ class Course {
+   static  int maxCapacity=100;
+    String courseName;
+    int enrollments;
 
-public class Course {
+     String[] enrolledStudents;
+
+    Course(String courseName){
+        this.courseName=courseName;
+        this.enrollments=0;
+        this.enrolledStudents= new String[maxCapacity];
+    }
+
+
+
+    static void setMaxCapacity(int maxCapacity){
+        Course.maxCapacity=maxCapacity;
+    }
+
+    void setEnrolledStudents(String studentName){
+        enrolledStudents[enrollments]=studentName;
+        enrollments++;
+    }
+
+    void unenrollStudent(String studentName){
+        System.out.println("Student Removed");
+        enrollments--;
+
+    }
 }
