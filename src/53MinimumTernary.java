e124d630d06786cfa6df28e0880048fd60ef671d
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-18 02:19:48 +0530

    .

--- src/53MinimumTernary.java
+++ src/53MinimumTernary.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class MinimumTernary {
+class MinimumTernary {
+     static void main() {
+         Scanner input=new Scanner(System.in);
+         System.out.println("Welcome to finding minimum");
+         System.out.print("please enter your first  number: ");
+         int num1=input.nextInt();
+         System.out.print("please enter your second  number: ");
+         int num2=input.nextInt();
+          MinimumTernary ternary=new MinimumTernary();
+          int min=ternary.min(num1,num2);
+
+         System.out.println("your minimum nunber is :"+min);
+     }
+
+     public int min(int num1, int num2){
+         return  num1 < num2 ? num1 : num2;
+     }
 }
