6b932778422b8024ec63357c13752a7a8befa148
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-19 02:05:04 +0530

    .

---
+++ Lectures/7 Control Statements Math and String/src/MyCar.java
@@ -0,0 +1,37 @@
+public class MyCar {
+    int noOfWheels;
+    int noOfDoors;
+    int maxSpeed;
+    String name;
+    String modelNumber;
+    String company;
+
+    public MyCar(int noOfWheels, int noOfDoors, int maxSpeed, String name, String modelNumber, String company) {
+        this.noOfWheels = noOfWheels;
+        this.noOfDoors = noOfDoors;
+        this.maxSpeed = maxSpeed;
+        this.name = name;
+        this.modelNumber = modelNumber;
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Car{");
+        sb.append("noOfWheels=").append(noOfWheels);
+        sb.append(", noOfDoors=").append(noOfDoors);
+        sb.append(", maxSpeed=").append(maxSpeed);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", modelNumber='").append(modelNumber).append('\'');
+        sb.append(", company='").append(company).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        MyCar swift = new MyCar(4, 4, 120,
+                "Swift", "SW876", "Maruti");
+        System.out.println(swift);
+
+    }
+}
