797b2c614fdac1e29d19afe6f6fa4de92a2e8810
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-21 23:12:58 +0530

    .

--- Lectures/9 Abstraction and Polymorphism/src/in/kgcoding/abstraction/TestAbstraction.java
+++ Lectures/9 Abstraction and Polymorphism/src/in/kgcoding/abstraction/TestAbstraction.java
@@ -1,4 +1,11 @@
 package in.kgcoding.abstraction;
 
 public class TestAbstraction {
+    static void main() {
+//      Vehicle veh= new Vehicle(4);
+        Car car=new Car();
+        car.commute();
+        car.makeStartSound();
+        car.getSetGo();
+    }
 }
--- Lectures/9 Abstraction and Polymorphism/src/in/kgcoding/abstraction/Transport.java
+++ Lectures/9 Abstraction and Polymorphism/src/in/kgcoding/abstraction/Transport.java
@@ -1,4 +1,6 @@
 package in.kgcoding.abstraction;
 
+// in interface method must be abstract but in interface it is predefined in abstract ,public
 public interface Transport {
+    void getSetGo();
 }
