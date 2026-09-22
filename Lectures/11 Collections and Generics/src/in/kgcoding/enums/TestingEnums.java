202ea1574a97950f00f2dc3f828714e995891b89
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-23 02:42:33 +0530

    .

--- Lectures/11 Collections and Generics/src/in/kgcoding/enums/TestingEnums.java
+++ Lectures/11 Collections and Generics/src/in/kgcoding/enums/TestingEnums.java
@@ -1,4 +1,14 @@
 package in.kgcoding.enums;
 
 public class TestingEnums {
+    static void main() {
+        TrafficLight color=TrafficLight.RED;
+        color =TrafficLight.GREEN;
+
+        Grade grade=Grade.valueOf("D");
+        for (Grade value : Grade.values()) {
+            System.out.println(value);
+        }
+
+    }
 }
