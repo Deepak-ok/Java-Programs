778eff3718360afd3c608c51855ee350f7905327
^Deepak-ok <deepak.shishupal.tomar2003 git config --global user.email>
2026-09-26 22:40:24 +0530

    .

--- Lectures/13 Functional Programming/src/in/kgcoding/methodreference/TestingMethodReference.java
+++ Lectures/13 Functional Programming/src/in/kgcoding/methodreference/TestingMethodReference.java
@@ -1,4 +1,31 @@
 package in.kgcoding.methodreference;
 
-public class TEestingMethodInterface {
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+import static java.lang.IO.println;
+
+public class TestingMethodReference {
+    static void main() {
+        List<Integer> numbers= List.of(1,2,3,4,5);
+
+
+        int newSum2=numbers.stream()
+                .reduce(0,(a,b)-> a+b );
+        System.out.printf("\n Sum Using reduce is %d",newSum2);
+
+        numbers.stream()
+                .filter(num-> num%2 ==1)
+                .forEach(num-> System.out.println(num));
+
+        numbers.stream()
+                .filter(num-> num%2 ==1)
+                .forEach(System.out::println);
+
+//        int max=numbers.stream()
+//                .reduce(0,(a,b)-> a>b ? a : b );
+//        System.out.printf("\n max Using reduce is %d",max);
+    }
 }
+
